ab9abf1e09fd10f6e6cc3be0f0f57598ae08ae3d
Isaiah Gama <isaiahgama@umail.ucsb.edu>
2021-07-07 11:54:35 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+
+    <div>
+      <h3>
+        Team PEBKAC's Reminder App!
+      </h3>
+    </div>
+
+  )
+
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
