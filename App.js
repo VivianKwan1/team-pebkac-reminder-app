a9de28f192b4733933a4fd3f6185f92168f376ad
Tony Guan <t9guan@ucsd.edu>
2021-07-11 02:21:56 -0700

    sign in page without css

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import SignInScreen from './screens/SignInScreen';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+// const AppNavigator = createStackNavigator(
+//   {
+//     SignIn: SignInScreen,
+//   },
+//   {
+//     initialRouteName: "SignIn"
+//   }
+// );
+
+// const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
   return (
 
 
 
-    <div>
-      <h3>
-        Team PEBKAC's Reminder App!
-      </h3>
-    </div>
+    // <div>
+    //   <h3>
+    //     Team PEBKAC's Reminder App!
+    //   </h3>
+    // </div>
 
+    <SignInScreen></SignInScreen>
+
+    // <AppContainer />
   )
 
 }
---
+++ screens/SignInScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+import { Button, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+function SignInScreen(props) {
+    return (
+        <SafeAreaView>
+            <Button
+                title='X'
+            // onPress={() => this.props.navigation.goBack()}
+            />
+            <Text>Don't Have Account? Sign Up</Text>
+            <Text style={styles.emailText}>Email</Text>
+            <TextInput
+                style={styles.emailInput}
+                placeholder='Email Adress'
+            />
+
+            <Text style={styles.passwordText}>Password</Text>
+            <TextInput
+                style={styles.passwordInput}
+                placeholder='Password'
+                secureTextEntry={true}
+            />
+
+            <Button
+                title='Sign In'
+            />
+
+            <Button
+                title='Google'
+            />
+            <Button
+                title='Facebook'
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    emailInput: {
+        margin: 12,
+        borderWidth: 1,
+    },
+
+    passwordInput: {
+        margin: 12,
+        borderWidth: 1,
+    },
+
+    emailText: {
+        fontWeight: 'bold',
+    },
+
+    passwordText: {
+        fontWeight: 'bold',
+    },
+
+})
+
+export default SignInScreen;
