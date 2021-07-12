5bd993a1adadda12fbf544a9dfab53e2e655c065
Tony Guan <t9guan@ucsd.edu>
2021-07-12 01:58:00 -0700

    added notification permission request

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  useEffect(() => {
+    (async() => {
+      if(Platform.OS === 'ios'){
+        const {status} = Notifications.requestPermissionsAsync();
+        if(status !== 'granted'){
+          alert('not granted notification permissions');
+          return (
+            <View>
+              <Text>thing</Text>
+            </View>
+          )
+        }
+      }
+    })();
+  }, []);
+
   return (
 
 
-
-    <div>
-      <h3>
-        Team PEBKAC's Reminder App!
-      </h3>
-    </div>
+  <View>
+    <Text>Team PEBKAC's Reminder App!</Text>
+  </View>
+    
 
   )
 
