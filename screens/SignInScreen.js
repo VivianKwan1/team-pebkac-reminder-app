862acc64adbd2aa414396241c9293e0ae7c60a89
Alice Lu <a2lu@ucsd.edu>
2021-07-13 06:43:13 +0000

    Update SignInScreen.js

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,62 +1,96 @@
 import React from 'react';
 import { TextInput } from 'react-native';
-import { Button, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Button, SafeAreaView, Text, StyleSheet, Pressable } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-function SignInScreen({ navigation }) {
+function SignInScreen(props) {
     return (
         <SafeAreaView>
             <Button
                 title='X'
             // onPress={() => this.props.navigation.goBack()}
             />
-            <Text>Don't Have Account? Sign Up</Text>
-            <Text style={styles.emailText}>Email</Text>
+            <Text style={styles.texts}>Don't Have Account? Sign Up</Text>
+            <Text style={styles.accountText}>Email Address</Text>
             <TextInput
-                style={styles.emailInput}
-                placeholder='Email Adress'
+                style={styles.accountInput}
+                placeholder='  Email Address'
             />
 
-            <Text style={styles.passwordText}>Password</Text>
+            <Text style={styles.accountText}>Password</Text>
             <TextInput
-                style={styles.passwordInput}
-                placeholder='Password'
+                style={styles.accountInput}
+                placeholder='  Password'
                 secureTextEntry={true}
             />
 
-            <Button
-                title='Sign In'
-                onPress={() => navigation.navigate('HomeScreen')}
-            />
+            <Pressable style = {styles.signButton}>
+              <Text style = {styles.text}>Sign In
+              </Text>
+            </Pressable>
 
-            <Button
-                title='Google'
-            />
-            <Button
-                title='Facebook'
-            />
+            <Pressable style = {styles.googfbButton}>
+              <Text style = {styles.text}>Google
+              </Text>
+            </Pressable>
+
+            <Pressable style = {styles.googfbButton}>
+              <Text style = {styles.text}>Facebook
+              </Text>
+            </Pressable>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    emailInput: {
-        margin: 12,
-        borderWidth: 1,
+    texts: {
+        fontSize:20,
+        marginHorizontal: 10,
+        fontWeight: 'bold'
+    },
+    signButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 18,
+      elevation: 3,
+      backgroundColor: '#0080FF',
+      marginBottom: 15,
+      marginHorizontal:120,
+      borderRadius:50,
+      marginTop: 30
+    },
+    
+    googfbButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 18,
+      elevation: 3,
+      backgroundColor: '#0080FF',
+      marginBottom: 15,
+      marginHorizontal:120,
+      borderRadius:50
     },
 
-    passwordInput: {
-        margin: 12,
-        borderWidth: 1,
+    text: {
+    fontSize:18,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
     },
 
-    emailText: {
-        fontWeight: 'bold',
+    accountInput: {
+        margin: 5,
+        borderWidth: 2,
+        borderRadius:10,
+        fontSize:20,
+        backgroundColor: '#E0E0E0'
     },
 
-    passwordText: {
-        fontWeight: 'bold',
-    },
+    accountText: {
+        margin: 2,
+        fontSize:18,
+        marginHorizontal: 10
+    }
 
 })
 
