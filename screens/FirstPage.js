277578b2845d57c39412ae18eaccf8139b3412b5
alicelu <alu7878@gmail.com>
2021-07-12 23:35:48 -0700

    added styling to first 3 pages

--- screens/FirstPage.js
+++ screens/FirstPage.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Button, TextInput, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Button, TextInput, SafeAreaView, Text, StyleSheet, Pressable } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 
-function FirstPage({ navigation }) {
+function FirstPage(props) {
     return (
         <SafeAreaView>
             <Text style={styles.appName}>Reminder App Name</Text>
-            
-            <Button
-                title='Sign In'
-                onPress={() => navigation.navigate('SignInScreen')}
-            />
-
-            <Button
-                title='Sign Up'
-                onPress={() => navigation.navigate('SignUpScreen')}
-            />
+            <Pressable style = {styles.signInbutton}>
+              <Text style = {styles.text}>Sign In
+              </Text>
+            </Pressable>
+            <Pressable style = {styles.signUpbutton}>
+              <Text style = {styles.text}>Sign Up
+              </Text>
+            </Pressable>
         </SafeAreaView>
     );
 }
@@ -28,10 +26,41 @@ const styles = StyleSheet.create({
     appName: {
         fontWeight: 'bold',
         fontSize:60,
-        marginBottom:100,
+        marginBottom:400,
         textAlign: 'center'
     },
+    
+    signInbutton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 25,
+      paddingHorizontal: 32,
+      elevation: 3,
+      backgroundColor: '#0080FF',
+      marginBottom: 20,
+      marginHorizontal:120,
+      borderRadius:50
+    },
 
+    signUpbutton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 25,
+      paddingHorizontal: 32,
+      elevation: 3,
+      backgroundColor: '#0080FF',
+      marginBottom: 20,
+      marginHorizontal:110,
+      borderRadius:50
+    },
+
+    text: {
+    fontSize:25,
+    lineHeight: 27,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
 })
 
 export default FirstPage;
