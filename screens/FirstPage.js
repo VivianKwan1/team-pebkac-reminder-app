bf959215741b97bf88bd9cf82b7893bb54cc5bab
Isaiah Gama <isaiahgama@umail.ucsb.edu>
2021-07-15 10:47:53 -0700

    Added calendar page, Alice's styling changes, and Tony's grouping tasks page.

--- screens/FirstPage.js
+++ screens/FirstPage.js
@@ -1,37 +1,94 @@
 import React from 'react';
-import { Button, TextInput, SafeAreaView, Text, StyleSheet } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {Button, TextInput, SafeAreaView, Text, StyleSheet, Pressable, Image, Dimensions, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 
-function FirstPage({ navigation }) {
-    return (
-        <SafeAreaView>
-            <Text style={styles.appName}>Reminder App Name</Text>
-            
-            <Button
-                title='Sign In'
-                onPress={() => navigation.navigate('SignInScreen')}
-            />
+function FirstPage({navigation}) {
+  return (
+    <SafeAreaView style={styles.page}>
+      <Image source={require('../assets/firstPage1.png')} style = {styles.backImage1}/>
+      <Image source={require('../assets/firstPage2.png')} style = {styles.backImage2}/>
+      <Text style={styles.appName}>Reminder App Name</Text>
 
-            <Button
-                title='Sign Up'
-                onPress={() => navigation.navigate('SignUpScreen')}
-            />
-        </SafeAreaView>
-    );
+    <View style={styles.parent}>
+        <Pressable style={styles.signInbutton} onPress={() => navigation.navigate('SignInScreen')}>
+            <Text style={styles.text}>Sign In</Text>
+        </Pressable>
+
+        <Pressable style={styles.signUpbutton} onPress = {() => navigation.navigate('SignUpScreen')}>
+            <Text style={styles.text}>Sign Up</Text>
+        </Pressable>
+    </View>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({
-    appName: {
-        fontWeight: 'bold',
-        fontSize:60,
-        marginBottom:100,
-        textAlign: 'center'
-    },
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
 
-})
+const styles = StyleSheet.create({
+  appName: {
+    fontWeight: 'bold',
+    fontSize: height*0.07,
+    marginBottom: height*0.43,
+    marginTop: height*0.07,
+    textAlign: 'center',
+    color: 'white',
+    marginHorizontal: width*0.01,
+  },
+
+  signInbutton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: height*0.06,
+    backgroundColor: '#FAF9C7',
+    marginBottom: height*0.02,
+    marginHorizontal: width*0.05,
+    borderRadius: 50,
+    width: width*0.25,
+  },
+
+  signUpbutton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FAF9C7',
+    marginBottom: height*0.02,
+    //marginHorizontal: width*0.35,
+    borderRadius: 50,
+    height: height*0.06,
+    width: width*0.25
+  },
+
+  text: {
+    fontSize: 20,
+    lineHeight: 27,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'gray',
+  },
+  page: {
+    backgroundColor: 'green',
+    width: width,
+    height: height,
+  },
+  backImage1: {
+    width: '100%', height: '60%', flex: 1, resizeMode: 'stretch',
+    position:'absolute',
+    marginTop: height*0.3,
+    marginHorizontal: width*0.1
+  },
+  parent: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: height *0.19
+  },
+  backImage2: {
+    width: '40%', height: '50%', flex: 1, resizeMode: 'stretch',
+    position:'absolute',
+    marginTop: height*0.07
+  },
+});
 
 export default FirstPage;
---
+++ screens/TasksScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+import { Button, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+function TasksScreen({ navigation }) {
+    return (
+        <SafeAreaView>
+            <Text>Tasks Page!!!</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    accountInput: {
+        margin: 12,
+        borderWidth: 1,
+    },
+
+    accountText: {
+        margin: 5,
+        fontWeight: 'bold',
+    }
+
+})
+
+export default TasksScreen;
