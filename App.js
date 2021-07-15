69bfa5e6ca91fb80b20c32d59ed163ce9e0cf320
Tony Guan <t9guan@ucsd.edu>
2021-07-15 00:47:35 -0700

    created group tasks screen

--- App.js
+++ App.js
@@ -1,62 +1,56 @@
-import 'react-native-gesture-handler';
-import React, { useState, useEffect, useRef } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import * as Notifications from 'expo-notifications';
-import SignInScreen from './screens/SignInScreen';
-import FirstPage from './screens/FirstPage';
-import SignUpScreen from './screens/SignUpScreen';
-import HomeScreen from'./screens/HomeScreen';
+import "react-native-gesture-handler";
+import React, { useState, useEffect, useRef } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as Notifications from "expo-notifications";
+import SignInScreen from "./screens/SignInScreen";
+import FirstPage from "./screens/FirstPage";
+import SignUpScreen from "./screens/SignUpScreen";
+import HomeScreen from "./screens/HomeScreen";
+import GroupTasksScreen from "./screens/GroupTasksScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  // const [expoPushToken, setExpoPushToken] = useState('');
-
-  // useEffect(() => {
-  //   (async() => {
-  //     if(Platform.OS === 'ios'){
-  //       const {status} = Notifications.requestPermissionsAsync();
-  //       if(status !== 'granted'){
-  //         alert('not granted notification permissions');
-  //         return (
-  //           <View>
-  //             <Text>thing</Text>
-  //           </View>
-  //         )
-  //       }
-  //     }
-  //   })();
-  // }, []);
-
   return (
     <NavigationContainer>
-    {
-      <Stack.Navigator initialRouteName="FirstPage">
-        <Stack.Screen name="FirstPage" component={FirstPage} options={{ headerShown: false }}/>
-        <Stack.Screen name="SignInScreen" component={SignInScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-    </Stack.Navigator>
-    }
+      {
+        <Stack.Navigator initialRouteName="FirstPage">
+          <Stack.Screen
+            name="FirstPage"
+            component={FirstPage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignInScreen"
+            component={SignInScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUpScreen"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      }
     </NavigationContainer>
-
-  )
-
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
-import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
+import * as React from "react";
+import { SafeAreaView } from "react-native";
+import { BottomNavigation, Text } from "react-native-paper";
+import GroupTasksScreen from "./GroupTasksScreen";
 
 const HomeRoute = () => <Text>Home page!</Text>;
 
@@ -16,12 +18,27 @@ const SettingsRoute = () => <Text>Settings page!</Text>;
 const MyComponent = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'home', title: 'Home', icon: 'home' , color: '#b29c6f'},
-    { key: 'calendar', title: 'Calendar', icon: 'calendar-month' , color: '#b29c6f'},
-    { key: 'newtask', title: 'New Task', icon: 'pencil-plus' , color: '#b29c6f'},
-    { key: 'tasks', title: 'Tasks', icon: 'checkbox-marked-outline' , color: '#b29c6f'},
-    { key: 'health', title: 'Health', icon: 'heart' , color: '#b29c6f'},
-    { key: 'settings', title: 'Settings', icon: 'menu' , color: '#b29c6f'},
+    { key: "home", title: "Home", icon: "home", color: "#b29c6f" },
+    {
+      key: "calendar",
+      title: "Calendar",
+      icon: "calendar-month",
+      color: "#b29c6f",
+    },
+    {
+      key: "newtask",
+      title: "New Task",
+      icon: "pencil-plus",
+      color: "#b29c6f",
+    },
+    {
+      key: "tasks",
+      title: "Tasks",
+      icon: "checkbox-marked-outline",
+      color: "#b29c6f",
+    },
+    { key: "health", title: "Health", icon: "heart", color: "#b29c6f" },
+    { key: "settings", title: "Settings", icon: "menu", color: "#b29c6f" },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
@@ -42,4 +59,13 @@ const MyComponent = () => {
   );
 };
 
-export default MyComponent;
+function Labels(props) {
+  return (
+    <SafeAreaView style={{ flexWrap: "wrap" }}>
+      <GroupTasksScreen></GroupTasksScreen>
+      {MyComponent}
+    </SafeAreaView>
+  );
+}
+
+export default Labels;
