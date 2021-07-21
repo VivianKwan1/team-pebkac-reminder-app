c27c4057a994b57f52f375262221e02f9c2176e3
Tony Guan <t9guan@ucsd.edu>
2021-07-21 03:08:54 -0700

    added google sign in

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,138 +1,174 @@
-import React from 'react';
-import { TextInput } from 'react-native';
-import { Button, SafeAreaView, Text, StyleSheet, Pressable, Dimensions, View, Image } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from "react";
+import { TextInput } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  View,
+  Image,
+} from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import firebase from "firebase";
 
-function SignInScreen({navigation}) {
-    return (
-        <SafeAreaView style = {styles.page}>
-          <Image source={require('../assets/secondPage.png')} style = {styles.backImage}/>
-            <Pressable style = {styles.cancelButton} onPress = {() => navigation.navigate('FirstPage')}>
-              <Text style = {styles.cancelText}>X
-              </Text>
-            </Pressable>
-            <Text style={styles.texts}>Don't Have Account? Sign Up</Text>
-            <Text style={styles.accountText}>Email Address</Text>
-            <TextInput
-                style={styles.accountInput}
-            />
+import * as Google from "expo-google-app-auth";
 
-            <Text style={styles.accountText}>Password</Text>
-            <TextInput
-                style={styles.accountInput}
-                secureTextEntry={true}
-            />
+function SignInScreen({ navigation }) {
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        // androidClientId: YOUR_CLIENT_ID_HERE,
+        behavior: "web",
+        iosClientId:
+          "312108100120-g0fq8ie5tfgv0qbnudr189bf799qilrv.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
+      });
 
-            <Pressable style = {styles.signButton} onPress = {() => navigation.navigate('HomeScreen')}>
-              <Text style = {styles.text}>Sign In
-              </Text>
-            </Pressable>
+      if (result.type === "success") {
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  };
 
-            <Pressable style = {styles.googButton}>
-              <Text style = {styles.text}>Google
-              </Text>
-            </Pressable>
+  return (
+    <SafeAreaView style={styles.page}>
+      <Image
+        source={require("../assets/secondPage.png")}
+        style={styles.backImage}
+      />
+      <Pressable
+        style={styles.cancelButton}
+        onPress={() => navigation.navigate("FirstPage")}
+      >
+        <Text style={styles.cancelText}>X</Text>
+      </Pressable>
+      <Text style={styles.texts}>Don't Have Account? Sign Up</Text>
+      <Text style={styles.accountText}>Email Address</Text>
+      <TextInput style={styles.accountInput} />
 
-            <Pressable style = {styles.fbButton}>
-              <Text style = {styles.text}>Facebook
-              </Text>
-            </Pressable>
+      <Text style={styles.accountText}>Password</Text>
+      <TextInput style={styles.accountInput} secureTextEntry={true} />
 
-        </SafeAreaView>
-    );
+      <Pressable
+        style={styles.signButton}
+        onPress={() => navigation.navigate("HomeScreen")}
+      >
+        <Text style={styles.text}>Sign In</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.googButton}
+        onPress={() => this.signInWithGoogleAsync()}
+      >
+        <Text style={styles.text}>Google</Text>
+      </Pressable>
+
+      <Pressable style={styles.fbButton}>
+        <Text style={styles.text}>Facebook</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
 }
 
-const width = Dimensions.get('window').width
-const height = Dimensions.get('window').height
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-    texts: {
-        fontSize:20,
-        marginHorizontal: width*0.1,
-        marginTop: height*0.1,
-        marginBottom: height*0.01
-    },
-    signButton: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 18,
-      elevation: 3,
-      backgroundColor: '#8A873B',
-      marginBottom: 15,
-      marginHorizontal: width*0.1,
-      borderRadius:10,
-      marginTop: 30
-    },
-
-    cancelButton: {
-      alignItems: 'flex-end',
-      paddingVertical: 18,
-      elevation: 3,
-      marginBottom: 15,
-      marginHorizontal: width*0.1,
-      borderRadius:10
-    },
-    
-    googButton: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 18,
-      elevation: 3,
-      backgroundColor: '#8A873B',
-      marginBottom: 15,
-      marginHorizontal: width*0.1,
-      borderRadius:10
-    },
-
-    fbButton: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 18,
-      elevation: 3,
-      backgroundColor: '#8A873B',
-      marginBottom: 300,
-      marginHorizontal: width*0.1,
-      borderRadius:10
-    },
-
-    text: {
-    fontSize:18,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-    },
-
-    accountInput: {
-        marginHorizontal: width*0.1,
-        borderWidth: 1,
-        //borderRadius:10,
-        fontSize:20
-    },
-
-    accountText: {
-        fontWeight: 'bold',
-        margin: 10, 
-        fontSize:18,
-        marginHorizontal: width*0.1,
-    },
-
-    cancelText: {
-    fontSize:30,
-    fontWeight: 'bold',
-    color: 'red',
-    },
-    
-    page: {
-    backgroundColor: '#FFFFE2'
-    },
-
-    backImage: {
-    width: '90%', height: '60%', flex: 1, resizeMode: 'stretch', 
-    alignContent:'center',
-    position:'absolute',
-    marginTop: height*0.3
+  texts: {
+    fontSize: 20,
+    marginHorizontal: width * 0.1,
+    marginTop: height * 0.1,
+    marginBottom: height * 0.01,
+  },
+  signButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 18,
+    elevation: 3,
+    backgroundColor: "#8A873B",
+    marginBottom: 15,
+    marginHorizontal: width * 0.1,
+    borderRadius: 10,
+    marginTop: 30,
   },
 
-})
+  cancelButton: {
+    alignItems: "flex-end",
+    paddingVertical: 18,
+    elevation: 3,
+    marginBottom: 15,
+    marginHorizontal: width * 0.1,
+    borderRadius: 10,
+  },
+
+  googButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 18,
+    elevation: 3,
+    backgroundColor: "#8A873B",
+    marginBottom: 15,
+    marginHorizontal: width * 0.1,
+    borderRadius: 10,
+  },
+
+  fbButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 18,
+    elevation: 3,
+    backgroundColor: "#8A873B",
+    marginBottom: 300,
+    marginHorizontal: width * 0.1,
+    borderRadius: 10,
+  },
+
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+
+  accountInput: {
+    marginHorizontal: width * 0.1,
+    borderWidth: 1,
+    //borderRadius:10,
+    fontSize: 20,
+  },
+
+  accountText: {
+    fontWeight: "bold",
+    margin: 10,
+    fontSize: 18,
+    marginHorizontal: width * 0.1,
+  },
+
+  cancelText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "red",
+  },
+
+  page: {
+    backgroundColor: "#FFFFE2",
+  },
+
+  backImage: {
+    width: "90%",
+    height: "60%",
+    flex: 1,
+    resizeMode: "stretch",
+    alignContent: "center",
+    position: "absolute",
+    marginTop: height * 0.3,
+  },
+});
 
 export default SignInScreen;
