0fd011ab6de00f8f50ab314b8973659abd12244a
Vivian <vkwan@ucsd.edu>
2021-08-30 22:07:33 -0700

    filter button + confirmation popups

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,18 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactNative, TextInput } from 'react-native';
-import { Button, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { ScrollView, CheckBox, Button, Modal, Pressable, TouchableOpacity, View, Dimensions, SafeAreaView, Text, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-
-
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Icon } from 'react-native-elements';
+import DropDownPicker from 'react-native-dropdown-picker';
+import firebase from 'firebase';
 
 function CalendarScreen({ navigation }) {
+
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModalVisibility = () => {
+        setModalVisible(!isModalVisible);
+    };
+
+    const [isSelected, setSelection] = useState(false);
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState([]);
+    const [items, setItems] = useState([
+        {
+            label: 'Work', value: 'work',
+            icon: () => <Icon name='circle' size={20}
+                type='material-community' color='purple' />
+        }, //set colors to the labels color
+        {
+            label: 'School', value: 'school',
+            icon: () => <Icon name='circle' size={20}
+                type='material-community' color='blue' />
+        },
+        {
+            label: 'Social', value: 'social',
+            icon: () => <Icon name='circle' size={20}
+                type='material-community' color='green' />
+        }
+    ]); //replace with labels, how order? a-z? how is it sorted in the edit task screen?
+
+    const filter = () => {
+    }
+
     return (
-        <SafeAreaView>
-            <Calendar/>
+        <SafeAreaView style={styles.bgColor}>
+            <TouchableOpacity style={styles.filterButton}>
+                <View style={styles.view}>
+                    <Icon
+                        name='tune'
+                        type='material-community'
+                        color='white'
+                        size={30}
+                        style={styles.icon}
+                        onPress={() => toggleModalVisibility()} />
+                </View>
+            </TouchableOpacity>
+            <Calendar />
+            <Modal animationType="slide"
+                transparent visible={isModalVisible}
+                presentationStyle="overFullScreen"
+                onDismiss={() => toggleModalVisibility}>
+                <View style={styles.viewWrapper}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>
+                            Filter Labels
+                        </Text>
+                        {/* <ScrollView style={styles.checkboxContainer}>
+                            <View style={styles.checkboxRow}>
+                                <CheckBox
+                                    value={isSelected}
+                                    onValueChange={setSelection}
+                                    style={styles.checkbox}
+                                />
+                                <Icon name='circle' size={20}
+                                    type='material-community' color='blue' style = {styles.checkboxIcons}/>
+                                <Text style={styles.label}>School</Text>
+                            </View>
+                            <View style={styles.checkboxRow}>
+
+                                <CheckBox
+                                    value={isSelected}
+                                    onValueChange={setSelection}
+                                    style={styles.checkbox}
+                                />
+                                <Icon name='circle' size={20}
+                type='material-community' color='green' style = {styles.checkboxIcons}/>
+                                <Text style={styles.label}>Social</Text>
+                            </View>
+                            <View style={styles.checkboxRow}>
+
+                                <CheckBox
+                                    value={isSelected}
+                                    onValueChange={setSelection}
+                                    style={styles.checkbox}
+                                />
+                                <Icon name='circle' size={20}
+                type='material-community' color='purple' style = {styles.checkboxIcons}/>
+                                <Text style={styles.label}>Work</Text>
+                            </View>
+                            <View style={styles.checkboxRow}>
+                                <CheckBox
+                                    value={isSelected}
+                                    onValueChange={setSelection}
+                                    style={styles.checkbox}
+                                />
+                                <Icon name='circle' size={20}
+                type='material-community' color='red' style = {styles.checkboxIcons}/>
+                                <Text style={styles.label}>idk what else</Text>
+                            </View>
+                        </ScrollView> */}
+                        <SafeAreaView style={styles.labelFilter}>
+                            <SafeAreaView style={styles.dropdown2}>
+                                <DropDownPicker
+                                    arrow
+                                    open={open}
+                                    value={value}
+                                    items={items}
+                                    setOpen={setOpen}
+                                    setValue={setValue}
+                                    setItems={setItems}
+                                    placeholder="Select labels"
+                                    multiple={true}
+                                    min={0}
+                                    max={5} //replace with the number of labels they have -1
+                                    maxHeight={120}
+                                    dropDownDirection="AUTO"
+                                    style={styles.dropdown}
+                                    placeholderStyle={{ color: "grey" }}
+                                    dropDownContainerStyle ={{borderColor: "grey"}} />
+                            </SafeAreaView>
+                        </SafeAreaView>
+                        <View style={styles.buttonContainer}>
+                            <Pressable onPress={() => setModalVisible(!isModalVisible)} style={[styles.buttons, styles.cancelButton]}>
+                                <Text>Cancel</Text>
+                            </Pressable>
+                            <Pressable onPress={() => filter()} style={[styles.buttons, styles.applyButton]}>
+                                <Text>Apply</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </SafeAreaView>
     );
 }
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
     accountInput: {
@@ -23,8 +155,115 @@ const styles = StyleSheet.create({
     accountText: {
         margin: 5,
         fontWeight: 'bold',
-    }
+    },
+    bgColor: {
+        backgroundColor: '#709C6C'
+    },
+    filterButton: {
+        marginBottom: 10,
+        marginHorizontal: width * 0.05,
+        paddingTop: 20,
+        paddingBottom: 10,
+        borderColor: "white",
+        alignItems: "stretch",
+        alignContent: 'flex-end'
+    },
 
+    view: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: 'flex-end',
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        paddingTop: 20
+    },
+    buttons: {
+        margin: 15,
+        backgroundColor: '#709C6C',
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 18,
+        paddingVertical: 8,
+        borderRadius: 5
+    },
+    confirmButton: {
+        backgroundColor: '#80bc8c',
+    },
+    cancelButton: {
+        backgroundColor: '#feaeae',
+    },
+    modalText: {
+        fontSize: 15,
+        fontWeight: "bold",
+        marginBottom: 15,
+        padding: 10,
+    },
+    screen: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fff",
+    },
+    viewWrapper: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+    },
+    modalView: {
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: "45%",
+        left: "50%",
+        elevation: 5,
+        transform: [{ translateX: -(width * 0.4) },
+        { translateY: -90 }],
+        width: width * 0.8,
+        backgroundColor: "#fff",
+        borderRadius: 7,
+        paddingVertical: 20,
+    },
+    labelFilter: {
+        paddingHorizontal: 20,
+        flexDirection: "row",
+    },
+    dropdown: {
+        borderWidth: 1,
+        borderColor: 'grey'
+    },
+    dropdown2: {
+        flex: 0.8,
+        paddingLeft: 10
+    },
+    labelsText: {
+        paddingTop: 13,
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    checkboxContainer: {
+        flexDirection: "column",
+    },
+    checkboxRow: {
+        flexDirection: "row",
+    },
+    checkbox: {
+        alignSelf: "center",
+    },
+    label: {
+        // margin: 8,
+        paddingTop: 6,
+        paddingLeft: 5
+    },
+    checkboxIcons: {
+        marginTop: 6,
+        // marginRight
+    }
 })
 
 export default CalendarScreen;
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,113 +1,201 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput } from 'react-native';
-import { Dimensions, Image, ScrollView, TouchableOpacity, View, Button, SafeAreaView, Text, StyleSheet, Pressable } from 'react-native';
+import { Dimensions, Image, Modal, ScrollView, TouchableOpacity, View, Button, SafeAreaView, Text, StyleSheet, Pressable } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { useNavigation } from '@react-navigation/native';
-import { Icon } from 'react-native-elements'
+import { Icon } from 'react-native-elements';
+import { getAuth, signOut, deleteUser } from 'firebase/auth';
 
 function SettingsScreen() {
-    const navigation = useNavigation(); 
-    return (
+  const auth = getAuth();
+  
+const user = auth.currentUser;
+  const navigation = useNavigation();
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const [inputValue, setInputValue] = useState("");
+  const [displayInput, setDisplayInput] = useState(false);
+  const [text, setText] = useState("");
+  const [screen, setScreen] = useState("");
+
+  const toggleModalVisibility = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const confirm = (prompt, nav, display) => {
+    setText(prompt);
+    setScreen(nav);
+    setDisplayInput(display);
+    setModalVisible(!isModalVisible);
+  }
+
+  const logOut = () => {
+    confirm('log out?', 'FirstPage', false);
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
+  const deleteAccount = () => {
+    confirm('delete your account? All data will be permanently deleted.', 'FirstPage', true)
+    deleteUser(user).then(() => {
+      // User deleted.
+    }).catch((error) => {
+      // An error ocurred
+      // ...
+    });
+  }
+
+  return (
     <SafeAreaView style={styles.background}>
-        <ScrollView>
+      <ScrollView>
         <View style={styles.view}>
           <Pressable onPress={() => navigation.goBack('MenuScreen')}>
-          <Icon
+            <Icon
               name='arrow-left'
               type='material-community'
               color='white'
               size={30}
-              style={styles.backButton}/>
+              style={styles.backButton} />
           </Pressable>
           <Text style={styles.title}>Settings</Text>
         </View>
 
         <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+          <View style={styles.view}>
+            <Icon
               name='emoticon-excited'
               type='material-community'
               color='white'
-              size={35}
-              style={styles.icon}/>
-        <Text style={styles.text}>Appearance</Text>
-        </View>
+              size={30}
+              style={styles.icon} />
+            <Text style={styles.text}>Appearance</Text>
+          </View>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+          <View style={styles.view}>
+            <Icon
               name='bell'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Notifications</Text>
-        </View>
-          </TouchableOpacity>
+              style={styles.icon} />
+            <Text style={styles.text}>Notifications</Text>
+          </View>
+        </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+          <View style={styles.view}>
+            <Icon
               name='security'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Security and Privacy</Text>
-        </View>
-          </TouchableOpacity>
+              style={styles.icon} />
+            <Text style={styles.text}>Security and Privacy</Text>
+          </View>
+        </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+          <View style={styles.view}>
+            <Icon
               name='transit-connection-variant'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Connection</Text>
-        </View>
-          </TouchableOpacity>  
+              style={styles.icon} />
+            <Text style={styles.text}>Connection</Text>
+          </View>
+        </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+          <View style={styles.view}>
+            <Icon
               name='account-box-outline'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Account</Text>
-        </View>
-        </TouchableOpacity>  
+              style={styles.icon} />
+            <Text style={styles.text}>Account</Text>
+          </View>
+        </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+        <TouchableOpacity style={styles.button} onPress={() => confirm('delete all tasks? This action cannot be undone.', 'GroupTasksScreen', true)}>
+          <View style={styles.view}>
+            <Icon
               name='delete'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Delete All Tasks</Text>
-        </View>
-        </TouchableOpacity>  
+              style={styles.icon} />
+            <Text style={styles.text}>Delete All Tasks</Text>
+          </View>
+        </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
-        <View style={styles.view}>
-        <Icon
+        <TouchableOpacity style={styles.button} onPress={() => deleteAccount()}>
+        {/* <TouchableOpacity style={styles.button} onPress={() => confirm('delete your account? All data will be permanently deleted.', 'FirstPage', true)}> */}
+          <View style={styles.view}>
+            <Icon
+              name='account-cancel-outline'
+              type='material-community'
+              color='white'
+              size={30}
+              style={styles.icon} />
+            <Text style={styles.text}>Delete Account</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => logOut()}>
+        {/* <TouchableOpacity style={styles.button} onPress={() => confirm('log out?', 'FirstPage', false)}> */}
+          <View style={styles.view}>
+            <Icon
               name='logout'
               type='material-community'
               color='white'
               size={30}
-              style={styles.icon}/>
-        <Text style={styles.text}>Logout</Text>
+              style={styles.icon} />
+            <Text style={styles.text}>Logout</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
+      <Modal animationType="slide"
+        transparent visible={isModalVisible}
+        presentationStyle="overFullScreen"
+        onDismiss={() => toggleModalVisibility}>
+        <View style={styles.viewWrapper}>
+          <View style={styles.modalView}>
+          {/* <View style={[styles.modalView, displayInput ? styles.modalWithInput : styles.modalNoInput]}> */}
+            <Text style={styles.modalText}>
+              Are you sure you want to {text}
+            </Text>
+            <Conditional displayInput={displayInput} inputValue={inputValue} />
+            <View style={styles.buttonContainer}>
+              <Pressable onPress={() => setModalVisible(!isModalVisible)} style={[styles.buttons, styles.cancelButton]}>
+                <Text>Cancel</Text>
+              </Pressable>
+              <Pressable onPress={() => navigation.navigate(screen)} style={[styles.buttons, styles.confirmButton]}>
+                <Text>Confirm</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
-        </TouchableOpacity>  
-        </ScrollView>
-      </SafeAreaView>
-    );
+      </Modal>
+    </SafeAreaView>
+  );
+}
+
+function Conditional(props) {
+  if (props.displayInput == true) {
+    return (
+      <TextInput placeholder="Enter your password to confirm."
+        value={props.inputValue} style={styles.textInput}
+        onChangeText={(value) => setInputValue(value)} />
+    )
+  }
+  return null;
 }
 
 const width = Dimensions.get('window').width
@@ -115,71 +203,125 @@ const height = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
 
-     icon:{
-      paddingLeft:25
-    },
+  icon: {
+    paddingLeft: 25
+  },
 
-    backButton:{
-      paddingHorizontal: 20,
-      paddingTop: 5,
-    },
+  backButton: {
+    paddingHorizontal: 20,
+    paddingTop: 5,
+  },
 
-    button:{
-      borderWidth : 1.5,
-      borderRadius: 25,
-      marginBottom: 10,
-      marginHorizontal: width*0.05,
-      paddingVertical: 2,
-      borderColor:"white",
-      alignItems:"stretch"
-    },
-  
-    view:{
-      flexDirection: "row",
-      alignItems: "center",
-      alignContent: 'flex-start'
-    },
+  button: {
+    borderWidth: 1.5,
+    borderRadius: 25,
+    marginBottom: 10,
+    marginHorizontal: width * 0.05,
+    paddingVertical: 2,
+    borderColor: "white",
+    alignItems: "stretch"
+  },
 
-    background:{
-        backgroundColor: '#709c6c',
-        flex:1,
-    },
+  view: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: 'flex-start'
+  },
 
-    title:{
-      fontSize: 25,
-      fontWeight: '600',
-      color: 'white',
-      paddingHorizontal: 20,
-      paddingBottom: 35,
-      paddingTop: 30,
-    },
+  background: {
+    backgroundColor: '#709c6c',
+    flex: 1,
+  },
 
-    text: {
-      fontSize: 17,
-      color: 'white',
-      fontWeight: '500',
-      paddingHorizontal: 40,
-      paddingBottom: 20,
-      paddingTop: 20,
-    },
-  
+  title: {
+    fontSize: 25,
+    fontWeight: '600',
+    color: 'white',
+    paddingHorizontal: 20,
+    paddingBottom: 35,
+    paddingTop: 30,
+  },
 
-    optBorders: {
-      paddingHorizontal: 45,
-      paddingBottom: 20,
-      paddingTop: 20,
-    },
+  text: {
+    fontSize: 17,
+    color: 'white',
+    fontWeight: '500',
+    paddingHorizontal: 40,
+    paddingBottom: 20,
+    paddingTop: 20,
+  },
 
-    accountInput: {
-        margin: 12,
-        borderWidth: 1,
-    },
+  accountInput: {
+    margin: 12,
+    borderWidth: 1,
+  },
 
-    accountText: {
-        margin: 5,
-        fontWeight: 'bold',
-    }
-
+  accountText: {
+    margin: 5,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  buttons: {
+    margin: 10,
+    backgroundColor: '#709C6C',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderRadius: 5
+  },
+  confirmButton: {
+    backgroundColor: '#80bc8c',
+  },
+  cancelButton: {
+    backgroundColor: '#feaeae',
+  },
+  modalText: {
+    marginBottom: 10,
+    padding: 10,
+    // paddingTop: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+  },
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  viewWrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  modalView: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    elevation: 5,
+    transform: [{ translateX: -(width * 0.4) },
+    { translateY: -90 }],
+    // height: 210,
+    width: width * 0.8,
+    backgroundColor: "#fff",
+    borderRadius: 7,
+    paddingVertical: 20,
+  },
+  textInput: {
+    width: "80%",
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderColor: "rgba(0, 0, 0, 0.2)",
+    borderWidth: 1,
+    marginBottom: 8,
+  },
 })
 
 export default SettingsScreen;
