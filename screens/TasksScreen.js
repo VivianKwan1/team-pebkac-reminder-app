6694cfb5fb4f61be3eec86aee0b3b3faeff0d7c3
Alice Lu <a2lu@ucsd.edu>
2021-09-03 03:56:26 +0000

    fixed TasksScreen.js

--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -2,110 +2,165 @@ import React, {useState, Fragment} from 'react';
 import { Alert } from 'react-native';
 import { TextInput } from 'react-native';
 import { Button, SafeAreaView, Text, Dimensions, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { useNavigation } from "@react-navigation/native";
+import firebase from "firebase";
 
 var today = new Date();
 var todayString = today.getMonth()+1 +"/"+today.getDate()+"/"+ today.getFullYear();
-today = new Date(todayString);
+today = new Date(todayString)
 var yesterday = new Date(today);
 yesterday.setDate(yesterday.getDate()-1)
 var tomorrow = new Date(today);
 tomorrow.setDate(tomorrow.getDate()+1);
 const bgColor = '#406c34';
-
-var Tasks = [
-    {
-    Task: 'Walk the dog',
-    Label: '#0000FF',
-    Date: '08/27/2021',
-    Time: '8:00 AM',
-    },
-    {
-    Task: 'Midterm Paper',
-    Label: '#FF0000',
-    Date: '09/02/2021',
-    Time: '10:00 AM',
-    },
-    {
-    Task: 'Call Grandma',
-    Label: '#800080',
-    Date: '09/20/2021',
-    Time: '8:00 AM',
-    },
-    {
-    Task: 'Party Hard',
-    Label: '#74ee15',
-    Date: todayString,
-    Time: '8:00 AM',
-    },
-    {
-    Task: 'Throw trash out',
-    Label: '#74ee15',
-    Date: '09/02/2021',
-    Time: '8:00 AM',
-    },
-
-];
-
-function pastTasks() {
-    var today = new Date();
-    var tempTasks = [];
-    for(const x of Tasks){
-        if (new Date(x.Date+" "+ x.Time).getTime() <= yesterday.getTime()) {
-            tempTasks.push(x);
-        }
-    }
-    return tempTasks
-}
-
-function currTasks() {
-    var today = new Date();
-    var tempTasks = [];
-    for(const x of Tasks){
-        if (new Date(x.Date+" "+ x.Time).getTime() > yesterday.getTime())  {
-            if (new Date(x.Date+" "+ x.Time).getTime() < tomorrow.getTime()) {
-                tempTasks.push(x);
-            }
-        }
-    }
-    return tempTasks
-}
-
-function futureTasks() {
-    var today = new Date();
-    var tempTasks = [];
-    for(const x of Tasks){
-        if (new Date(x.Date+" "+ x.Time).getTime() > today.getTime()) {
-            tempTasks.push(x);
-        }
-    }
-    return tempTasks
-}
-
-var tempPast = pastTasks()
-
-var past = tempPast.sort((a,b) => {
-    return new Date(a.Date+" "+ a.Time).getTime() - 
-    new Date(b.Date+" "+b.Time).getTime()
-})
-
-var tempCurrent = currTasks()
-
-var current = tempCurrent.sort((a,b) => {
-    return new Date(a.Date+" "+ a.Time).getTime() - 
-    new Date(b.Date+" "+b.Time).getTime()
-})
-
-var tempFuture = futureTasks()
-
-var future = tempFuture.sort((a,b) => {
-    return new Date(a.Date+" "+ a.Time).getTime() - 
-    new Date(b.Date+" "+b.Time).getTime()
-})
+var myTasks = [];
+var tmpTask = {};
+var colorHex = '';
 
 function TasksScreen({ navigation }) {
+  const userId = firebase.auth().currentUser.uid;
+  firebase
+      .database()
+      .ref("users/" + userId + "/tasks")
+      .on("value", (tasks) => {
+        tasks.forEach((childTask) =>{
+  
+          //finding out which label it has
+          //personal
+          if (childTask.child('labels').child('personal').toJSON().toString() ==='true') {
+              colorHex = '#BFD0CA';
+          } //work
+          else if (childTask.child('labels').child('work').toJSON().toString() === 'true') {
+              colorHex = '#84ad75';
+          } //school
+          else if (childTask.child('labels').child('school').toJSON().toString() ==='true') {
+              colorHex = '#FF0000';
+          } //social
+          else if (childTask.child('labels').child('social').toJSON().toString() ==='true') {
+              colorHex = '#0F4C81';
+          }
+  
+          //
+          tmpTask = {
+              Task: childTask.child('taskName').val(),
+              Label: colorHex,
+              Date: childTask.child('date').val(),
+              Time: childTask.child('time').val()
+          }
+  
+          myTasks.push(tmpTask)
+  
+        });
+        
+      });
+  
+      var markedDatesArray = [];
+
+      function setMarkedDatesArray(){
+          if(myTasks === []){
+              return [];
+          }
+          else{
+              for(x in myTasks){
+  
+                  var temp = {
+                                  date: myTasks[x].Date,
+                                  dots: [
+                                        {
+                                        color: myTasks[x].Label
+                                        }
+                                        ]
+                              };
+  
+                  markedDatesArray.push(temp);
+              }
+          }
+      }
+  
+      function combineLabels(){
+          var date = [];
+          var tags = [];
+          //for each task, find out if there is another task with the same day. Tag/Day is pushed to tags if there are.
+          for(x in myTasks){
+              if(date.includes(myTasks[x].Date)){
+                  tags.push([myTasks[x].Label,myTasks[x].Date]);
+              }else{
+                  date.push(myTasks[x].Date);
+              }
+          }
+          //for each task with the same day, adjust the tag in the colors array.
+          for( i in tags){
+              for(j in markedDatesArray){
+                  if(markedDatesArray[j].date == tags[i][1]){
+  
+                      if(markedDatesArray[j].dots[0].color == tags[i][0]){
+                          continue;
+                      }else{
+                          markedDatesArray[j].dots.push({color: tags[i][0]});
+                      }
+                  }
+              }
+          }
+      }
+  
+      setMarkedDatesArray();
+      combineLabels();
+  
+  function pastTasks() {
+      var tempTasks = [];
+      for(const x of myTasks){
+          if (new Date(x.Date).getTime() < today.getTime()) {
+              tempTasks.push(x);
+          }
+      }
+      return tempTasks
+  }
+  
+  function currTasks() {
+      var tempTasks = [];
+      for(const x of myTasks){
+          if (new Date(x.Date).getTime() >= today.getTime())  {
+            if (new Date(x.Date).getTime() < tomorrow.getTime()) {
+              tempTasks.push(x);
+            }
+          }
+      }
+      return tempTasks
+  }
+  
+  function futureTasks() {
+      var tempTasks = [];
+      for(const x of myTasks){
+          if (new Date(x.Date).getTime() >= tomorrow.getTime()) {
+              tempTasks.push(x);
+          }
+      }
+      return tempTasks
+  }
+  
+  var tempPast = pastTasks()
+  
+  var past = tempPast.sort((a,b) => {
+      return new Date(a.Date+" "+ a.Time).getTime() - 
+      new Date(b.Date+" "+b.Time).getTime()
+  })
+  
+  var tempCurrent = currTasks()
+  
+  var current = tempCurrent.sort((a,b) => {
+      return new Date(a.Date+" "+ a.Time).getTime() - 
+      new Date(b.Date+" "+b.Time).getTime()
+  })
+  
+  var tempFuture = futureTasks()
+  
+  var future = tempFuture.sort((a,b) => {
+      return new Date(a.Date+" "+ a.Time).getTime() - 
+      new Date(b.Date+" "+b.Time).getTime()
+  })
     var dateReturned = [];
-    return (
+  return (
         <ScrollView style={styles.container}>
             <Text style = {styles.todayText}>Today</Text>
             {current.map((prop) => {
@@ -161,7 +216,7 @@ const styles = StyleSheet.create({
     backgroundColor: bgColor,
   },
   button: {
-    opacity: 0.1,
+    activeOpacity: 0.1,
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
